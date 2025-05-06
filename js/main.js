ccdef691fd70668ccea12e1dbc99f61a2c14b2c6
Liz96c <liza-camacho@hotmail.com>
2025-05-05 22:31:03 -0600

    Subiendo archivo js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*Primero crearás una variable llamada ejemplo y le
+asignarás como valor una función. La misma debe retornar
+un string, que diga: “Hola, soy una función
+expresada”.*/
+
+let ejemplo=function ejemplo(){
+    return 'Hola, soy una función';
+ }
+console.log(ejemplo)
